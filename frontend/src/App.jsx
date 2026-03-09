623087ee73919241c815375a44839fe3c4dbd4b5
Neeraj Negi <neerajnegi108333@gmail.com>
2026-03-09 11:38:03 +0530

    Integrate Footer into main layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import Home from './pages/Home'
 import Detection from './pages/Detection'
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detect" element={<Detection />} />
-      </Routes>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/detect" element={<Detection />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   )
 }
